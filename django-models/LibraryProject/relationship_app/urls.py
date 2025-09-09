94d1825116fe8a2271f2368057ee9a42a2594929
precious thomas <preshusthomas28@gmail.com>
2025-09-09 14:51:30 +0100

    Added relationship_app with list books

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,3 +20,11 @@ urlpatterns += [
     path("books/edit/", views.edit_book, name="edit_book"),
     path("books/delete/", views.delete_book, name="delete_book"),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns += [
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+]
