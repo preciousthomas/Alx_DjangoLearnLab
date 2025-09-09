ef39326a8d7984f1ade5e37e0b8ed7f3a7229c68
precious thomas <preshusthomas28@gmail.com>
2025-09-09 11:43:45 +0100

    Added relationship_app with list books

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -17,10 +17,3 @@ class LibraryDetailView(DetailView):
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
-
-
-# Class-based view
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
