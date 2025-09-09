76aa50c4a734f78593461a9ef50d2cbca0838319
precious thomas <preshusthomas28@gmail.com>
2025-09-09 14:00:17 +0100

    Added relationship_app with list books

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,31 +1,16 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
+from django.views.generic import DetailView
+from .models import Book, Library
 
 
-# --- Role checks ---
-def is_admin(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-def is_librarian(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
-
-
-def is_member(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
-
-
-# --- Role-based views ---
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, "relationship_app/admin_view.html")
-
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, "relationship_app/librarian_view.html")
-
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, "relationship_app/member_view.html")
+# Class-based view to show library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
