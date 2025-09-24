1525749365b551876bad9313e568b3843296b3e2
precious thomas <preshusthomas28@gmail.com>
2025-09-24 18:02:54 +0100

    advanced project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework import permissions
-from .models import Book
-from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book  # Import the Book model
 # ---------------------------
 # List all books (public)
 # ---------------------------
