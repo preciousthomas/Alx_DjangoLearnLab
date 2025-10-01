c086ca5e92bb7417867c5c0ae44ba148fd200e68
precious thomas <preshusthomas28@gmail.com>
2025-10-01 14:13:17 +0100

    django_blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
