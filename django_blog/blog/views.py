d3c156c54a2945b7f3f7d3a88334eb7813cb258b
precious thomas <preshusthomas28@gmail.com>
2025-10-01 14:43:40 +0100

    django_blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -242,3 +242,20 @@ class PostSearchView(ListView):
         ctx['query'] = self.request.GET.get('q', '')
         return ctx
 post.pk})
+    from django.db.models import Q
+from django.shortcuts import render
+from .models import Post
+
+
+def search_posts(request):
+    query = request.GET.get('q')
+    results = Post.objects.all()
+
+    if query:
+        results = results.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(tags__name__icontains=query)   # ✅ search inside tags
+        ).distinct()
+
+    return render(request, 'blog/search_results.html', {'results': results, 'query': query})
