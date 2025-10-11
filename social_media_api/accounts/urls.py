d35e51b084cd9a2b106885a5f96f3383c97281ff
precious thomas <preshusthomas28@gmail.com>
2025-10-11 08:39:45 +0100

    social media

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from .views import RegisterView, LoginView, UserProfileView
 
@@ -5,4 +6,11 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+]
+from django.urls import path, include
+path('api/', include('posts.urls')),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/', include('posts.urls')),  # ✅ this is what the check looks for
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -40,3 +40,23 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+        from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
