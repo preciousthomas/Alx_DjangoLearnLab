0427f92576b9e7c06d5865adce29de30bd2c2887
precious thomas <preshusthomas28@gmail.com>
2025-10-11 14:26:29 +0100

    social media

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -8,3 +8,9 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    following = models.ManyToManyField(
+    'self',
+    symmetrical=False,
+    related_name='followers',
+    blank=True
+)
