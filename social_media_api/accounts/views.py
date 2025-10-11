6c91aaf2cb8c51a96280737d2460efeaf4a6dc7b
precious thomas <preshusthomas28@gmail.com>
2025-10-11 14:42:08 +0100

    social media

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -99,3 +99,17 @@ class UnfollowUserView(APIView):
 
         request.user.following.remove(target_user)
         return Response({"detail": f"You have unfollowed {target_user.username}."}, status=status.HTTP_200_OK)
+    
+    from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
+
+
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({"detail": f"You are now following {user_to_follow.username}."}, s
